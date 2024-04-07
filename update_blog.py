fe9788e9d21fd119105cc20029e2209aab91785c
Jonathan Evans <JonathanEvansDev@gmail.com>
2024-04-07 11:56:32 -0500

    Add support for style in blog posts

--- update_blog.py
+++ update_blog.py
@@ -13,22 +13,30 @@ import shutil
 from dateutil import parser
 from bs4 import BeautifulSoup, Tag
 
-import bs4
+
+HTMLID = typing.NewType("HTMLID", str)
 
 BLOG_POST_DATA_FOLDER = pathlib.Path("./blog_posts")
 BLOG_POST_PAGE_FOLDER = pathlib.Path("./blog")
 BLOG_LIST_PAGE = pathlib.Path("./index.html")
+BLOG_POST_TEMPLATE_PAGE = pathlib.Path("blog_post_template.html")
 
-POST_LIST_ID = "post_list"
+POST_LIST_ID = HTMLID("post_list")
 
 POST_HEADER_DELIMITER = "---\n"
 
 TAG_KEY = "tags"
 PUBLISH_DATE_KEY = "publish date"
-EXPECTED_KEYS = (TAG_KEY, PUBLISH_DATE_KEY)
+REQUIRED_KEYS = (TAG_KEY, PUBLISH_DATE_KEY)
 
+POST_TITLE_ID = HTMLID("post_title")
+
+POST_CONTENT_ID = HTMLID("post_content")
+
+SUBTITLE_KEY = "subtitle"
 
 PostTitle = typing.NewType("PostTitle", str)
+PostSubtitle = typing.NewType("PostSubtitle", str)
 PostBody = typing.NewType("PostBody", str)
 PostTag = typing.NewType("PostTag", str)
 
@@ -37,27 +45,33 @@ def _string_iterable_to_stream(strings: typing.Iterable[str]) -> io.StringIO:
     return io.StringIO("".join(strings))
 
 
-def _parse_as_yaml(lines: typing.Iterable[str]) -> typing.Mapping[str, typing.Any]:
+def _parse_as_yaml(lines: typing.Iterable[str]) -> typing.Mapping[str, str]:
     return yaml.safe_load(_string_iterable_to_stream(lines))
 
 
-def _has_expected_keys(header_data: typing.Mapping[str, typing.Any]) -> bool:
-    return all(key in header_data for key in EXPECTED_KEYS)
+def _has_required_keys(header_data: typing.Mapping[str, str]) -> bool:
+    return all(key in header_data for key in REQUIRED_KEYS)
 
 
 @dataclasses.dataclass(frozen=True)
 class PostMetadata:
     tags: typing.Sequence[PostTag]
     publish_date: datetime.datetime
+    subtitle: typing.Optional[PostSubtitle]
 
     @staticmethod
     def from_post_header_text(header_lines: typing.Iterable[str]) -> PostMetadata:
         header_data = _parse_as_yaml(header_lines)
-        assert _has_expected_keys(header_data)
-        return PostMetadata(
-            [PostTag(tag) for tag in header_data[TAG_KEY]],
-            parser.parse(header_data[PUBLISH_DATE_KEY]),
-        )
+        assert _has_required_keys(header_data)
+
+        tags = [PostTag(tag) for tag in header_data[TAG_KEY]]
+        publish_date = parser.parse(header_data[PUBLISH_DATE_KEY])
+        if SUBTITLE_KEY in header_data:
+            subtitle = PostSubtitle(header_data[SUBTITLE_KEY])
+        else:
+            subtitle = None
+
+        return PostMetadata(tags, publish_date, subtitle)
 
 
 def _is_header_delimiter(text: str) -> bool:
@@ -137,8 +151,44 @@ class BlogPostPage:
     page_path: pathlib.Path
 
     @staticmethod
-    def _make_html(blog_post: BlogPost) -> HTMLstr:
-        return HTMLstr(markdown.markdown("".join(blog_post.body)))
+    def _set_text_by_id(html: BeautifulSoup, content: str, id_: HTMLID):
+        element = html.find(attrs={"id": id_})
+        assert isinstance(element, Tag)
+        element.string = content
+
+    @staticmethod
+    def _set_post_title(html: BeautifulSoup, title: PostTitle, id_: HTMLID):
+        BlogPostPage._set_text_by_id(html, title, id_)
+
+    @staticmethod
+    def _set_post_subtitle(html: BeautifulSoup, title: PostSubtitle, id_: HTMLID):
+        BlogPostPage._set_text_by_id(html, title, id_)
+
+    @staticmethod
+    def _set_post_content(html: BeautifulSoup, content: HTMLstr, post_body_id_: HTMLID):
+        post_body_element = html.find(attrs={"id": post_body_id_})
+
+        assert isinstance(post_body_element, Tag)
+        post_body_element.append(BeautifulSoup(content, "html.parser"))
+
+    @staticmethod
+    def _make_html(blog_post: BlogPost, template_html: BeautifulSoup) -> HTMLstr:
+        BlogPostPage._set_post_title(template_html, blog_post.post_title, POST_TITLE_ID)
+
+        if blog_post.metadata.subtitle is not None:
+            BlogPostPage._set_post_subtitle(
+                template_html, blog_post.metadata.subtitle, POST_TITLE_ID
+            )
+
+        blog_post_content = HTMLstr(markdown.markdown("".join(blog_post.body)))
+
+        BlogPostPage._set_post_content(
+            template_html, blog_post_content, POST_CONTENT_ID
+        )
+
+        blog_post_page = HTMLstr(str(template_html.prettify()))
+
+        return blog_post_page
 
     @staticmethod
     def _get_path(blog_post: BlogPost) -> pathlib.Path:
@@ -151,11 +201,17 @@ class BlogPostPage:
 
     @staticmethod
     def from_blog_post(
-        blog_post: BlogPost, blog_post_root_folder: pathlib.Path
+        blog_post: BlogPost,
+        blog_post_root_folder: pathlib.Path,
+        template_page_path: pathlib.Path,
     ) -> BlogPostPage:
+
+        with open(template_page_path, "r") as f:
+            template_page = BeautifulSoup(f, "html.parser")
+
         return BlogPostPage(
             blog_post,
-            BlogPostPage._make_html(blog_post),
+            BlogPostPage._make_html(blog_post, template_page),
             blog_post_root_folder / BlogPostPage._get_path(blog_post),
         )
 
@@ -184,7 +240,7 @@ class BlogPostPage:
             {self.source_data.metadata.publish_date.strftime("%Y/%m/%d")}
         </div>
     </div>
-    <p class="blurb">{"Hello"}</p>
+    {'' if self.source_data.metadata.subtitle is None else f'<p class="blurb">{self.source_data.metadata.subtitle}</p>'}
 </li>""",
             "html.parser",
         )
@@ -212,7 +268,11 @@ def generate_blog_post_pages(
     for blog_post in rich.progress.track(
         blog_posts, "Generating posts", show_speed=True
     ):
-        page = BlogPostPage.from_blog_post(blog_post, blog_post_root_folder)
+        page = BlogPostPage.from_blog_post(
+            blog_post,
+            blog_post_root_folder,
+            BLOG_POST_TEMPLATE_PAGE,
+        )
         page.write_file()
         pages.append(page)
 
@@ -250,7 +310,9 @@ def overwrite_list_page_html(
 
     post_list_element.clear()
     add_post_break(post_list_element)
-    for page in pages:
+    for page in rich.progress.track(
+        pages, description="Updating Homepage", show_speed=True
+    ):
         post_list_element.append(page.make_bs4_card_tag())
         add_post_break(post_list_element)
 
